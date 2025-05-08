3375907fdc11771b45fec2a40597c9b8de5de1b1
DmytroProg <dmytroprog@gmail.com>
2025-05-08 20:21:31 +0300

    added order service and repository

---
+++ CafeApp.Core/DTOs/Inputs/AddOrderDto.cs
@@ -0,0 +1,11 @@
+﻿using CafeApp.Core.Models;
+
+namespace CafeApp.Core.DTOs.Inputs;
+
+public class AddOrderDto
+{
+    public string Address { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+    public int CustomerId { get; set; }
+}
---
+++ CafeApp.Core/Interfaces/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using CafeApp.Core.Models;
+
+namespace CafeApp.Core.Interfaces;
+
+public interface IOrderRepository
+{
+    Task<Order> PlaceOrderAsync(Order order, CancellationToken cancellationToken);
+}
