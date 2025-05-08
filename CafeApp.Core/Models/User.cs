43cb7e9e36b3f291971d31a80a281d34a905384c
DmytroProg <dmytroprog@gmail.com>
2025-05-08 19:02:12 +0300

    added models

---
+++ CafeApp.Core/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CafeApp.Core.Models;
+
+public abstract class User : EntityChanges
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string FullName => $"{LastName} {FirstName}";
+
+    public DateTime BirthDate { get; set; }
+}
