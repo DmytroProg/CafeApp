bceb242a7b4eea57d665edb46b280fc0aa1f0984
DmytroProg <dmytroprog@gmail.com>
2025-05-13 18:59:12 +0300

    added ef core support and repository for order

---
+++ CafeApp.Storage/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using CafeApp.Core.Interfaces;
+using CafeApp.Core.Models;
+
+namespace CafeApp.Storage.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly CafeAppContext _context;
+
+    public OrderRepository(CafeAppContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Order> PlaceOrderAsync(Order order, CancellationToken cancellationToken)
+    {
+        _context.Orders.Add(order);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return order;
+    }
+}
